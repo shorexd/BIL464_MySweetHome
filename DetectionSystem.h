197d28d465017628a3f35b44776bac67c056dfe6
kayra000 <152815384+kayra000@users.noreply.github.com>
2025-12-04 23:07:57 +0300

    DetectionSystem sinifi ve Yangin Senaryosu zinciri eklendi (LLR-04, LLR-06)

---
+++ DetectionSystem.h
@@ -0,0 +1,27 @@
+/**
+ * DOSYA ADI DetectionSystem.h
+ * ROLÜ: Somut Gözlemci (Concrete Observer) - YANGIN/GAZ İÇİN
+ * GÖREVİ: Duman/Gaz algılandığında Algılama Zincirini (Detection Chain) tetikler.
+ * İLGİLİ LLR LLR-04, LLR-06
+ */
+
+#ifndef DETECTIONSYSTEM_H
+#define DETECTIONSYSTEM_H
+
+#include "IObserver.h"
+#include <string>
+
+class DetectionSystem : public IObserver {
+public:
+    DetectionSystem();
+    ~DetectionSystem();
+
+    // Sensörden haber alma fonksiyonu
+    void update(const std::string& event_type) override;
+
+private:
+    // Yangın senaryosunu (Alarm->Flaşör->İtfaiye) başlatacak fonksiyon
+    void executeDetectionChain();
+};
+
+#endif // DETECTIONSYSTEM_H
